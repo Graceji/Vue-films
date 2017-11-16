206916dd7297466af8eb72f84d5731441e2b0bc6
Grace_ji <80709412@qq.com>
2017-11-16 21:24:41 +0800

    添加导航

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,31 @@
 <template>
   <el-container>
     <el-header>
-      
+      <div class="logo">
+        <i class="el-icon-star-off"></i>
+        <span class="logo-name">疯人电影院</span>
+      </div>
+      <el-input v-model="input" placeholder="请输入内容" prefix-icon="el-icon-search"></el-input>
+      <!-- <el-menu
+        :default-active="activeIndex2"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <el-menu-item index="1">处理中心</el-menu-item>
+        <el-submenu index="2">
+          <template slot="title">我的工作台</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
+        </el-submenu>
+        <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+        <el-menu-item>
+          
+        </el-menu-item>
+      </el-menu> -->
     </el-header>
     <el-container>
       <el-aside width="200px">Aside</el-aside>
@@ -14,11 +38,33 @@
 </template>
 
 <script>
+  export default {
+    data () {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      }
+    },
+    methods: {
+      handleSelect (key, keyPath) {
+        console.log(key, keyPath)
+      }
+    }
+  }
 </script>
 
-<style>
+<style lang="scss">
 .el-header {
-  background-color: #FF8040;
+  background-color: #545c64;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .logo {
+    color: #fff;
+  }
+  .el-input {
+    width: 500px;
+  }
 }
 
 .el-footer {
